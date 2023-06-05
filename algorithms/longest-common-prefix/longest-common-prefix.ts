f9fda6a3e2a0fac5c322ad80b9c2ad9464b8ed97
marcin.czepiela <marcin.czepiela@humanit.group>
2023-06-05 20:39:53 +0200

    Add solution for longest-common-prefix algorithm

--- algorithms/longest-common-prefix/longest-common-prefix.ts
+++ algorithms/longest-common-prefix/longest-common-prefix.ts
@@ -1,3 +1,21 @@
 export function longestCommonPrefix(strs: string[]): string {
-  return ''
+  let prefix = strs[0]
+
+  for (let i = 1; i < strs.length; i++) {
+    const str = strs[i]
+
+    let j = 0
+    while (j < prefix.length) {
+      if (str[j] !== prefix[j]) {
+        prefix = prefix.slice(0, j)
+        break
+      }
+      j++
+    }
+  }
+
+  return prefix
 }
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
